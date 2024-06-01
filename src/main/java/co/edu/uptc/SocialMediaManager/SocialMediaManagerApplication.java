646a06f6165b597cb63f836650f05b1456face91
Diego Combariza <136619052+DiegoCombariza11@users.noreply.github.com>
2024-06-01 16:04:57 -0500

    Revert "Feature/GRS-24"

--- src/main/java/co/edu/uptc/SocialMediaManager/SocialMediaManagerApplication.java
+++ src/main/java/co/edu/uptc/SocialMediaManager/SocialMediaManagerApplication.java
@@ -2,25 +2,12 @@ package co.edu.uptc.SocialMediaManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SocialMediaManagerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SocialMediaManagerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaManagerApplication.class, args);
+	}
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/static/pages/**")
-                        .addResourceLocations("classpath:/static/pages/");
-            }
-        };
-    }
 }
